a5133dc875e4e35ced6a5110abe9d8b9955db40b
Leon Kuijf <leon.kuijf@gmail.com>
2023-08-01 09:58:35 +0200

    added some nessecery files

---
+++ config/app_wt.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | W.T. specific configurations
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'cmsPath' => 'https://aanmelden.wtgroup.nl/_mcfu638b-cms', // used by ApiCall-helper and image rendering
+    'metaTitle' => 'WT Aanmeld-formulier', // Default title when none is entered.
+    'metaDescription' => 'Aanmelden voor werknemers WT Group', // Default description when none is entered.
+];
