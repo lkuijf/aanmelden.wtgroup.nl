09c6bad70dd9b556ba6c5f29a74349505b13e2d4
Leon Kuijf <leon.kuijf@gmail.com>
2023-08-03 13:29:09 +0200

    default values for more error proof

--- database/migrations/2023_08_01_094243_create_registrations_table.php
+++ database/migrations/2023_08_01_094243_create_registrations_table.php
@@ -15,19 +15,19 @@ return new class extends Migration
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
-            $table->string('full_name');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->date('birth_date');
-            $table->string('email');
-            $table->string('company');
-            $table->text('diet_wishes');
-            $table->boolean('partner');
-            $table->string('partner_name');
+            $table->string('full_name')->default('');
+            $table->string('first_name')->default('');
+            $table->string('last_name')->default('');
+            $table->date('birth_date')->default('');
+            $table->string('email')->default('');
+            $table->string('company')->default('');
+            $table->text('diet_wishes')->default('');
+            $table->boolean('partner')->default(0);
+            $table->string('partner_name')->default('');
             $table->integer('children_amount')->default(0);
-            $table->string('children_ages');
+            $table->string('children_ages')->default('');
             $table->integer('page_id')->nullable()->default(null);
-            $table->string('page_slug_at_registration');
+            $table->string('page_slug_at_registration')->default('');
             $table->timestamps();
         });
     }
